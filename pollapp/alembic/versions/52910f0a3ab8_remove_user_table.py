8462f7f464bdbe66687c0339943f9faa6d61b893
JohnTer <dj.vadim@list.ru>
2021-06-16 17:09:03 +0300

    Remove user table

---
+++ pollapp/alembic/versions/52910f0a3ab8_remove_user_table.py
@@ -0,0 +1,24 @@
+"""remove user table
+
+Revision ID: 52910f0a3ab8
+Revises: e77b5f01bd84
+Create Date: 2021-06-16 17:07:14.224435
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '52910f0a3ab8'
+down_revision = 'e77b5f01bd84'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- pollapp/db/models.py
+++ pollapp/db/models.py
@@ -3,13 +3,6 @@ from sqlalchemy.dialects.postgresql import JSON
 from . import db
 
 
-class User(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.String(), primary_key=True)
-    nickname = db.Column(db.Integer())
-
-
 class Poll(db.Model):
     __tablename__ = 'polls'
 
